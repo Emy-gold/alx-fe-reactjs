706f7d315d020216af1f803590b182e36e2e0b73
Emy-gold <imanelgarate@gmail.com>
2025-07-20 17:04:16 +0100

    add the file and add the app

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
 import MainContent from './components/MainContent.jsx';
+import UserProfile from './components/UserProfile.jsx';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <>
       <Header />
       <MainContent />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Footer />
     </>
   );
