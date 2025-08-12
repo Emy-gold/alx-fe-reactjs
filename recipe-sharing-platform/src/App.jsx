edb381fbf4561645edb082cf9cbc0f099c4ba29f
Emy-gold <imanelgarate@gmail.com>
2025-08-12 22:26:12 +0100

    add

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./components/HomePage.jsx"
 
 export const App = () => {
   return (
-    <div className="text-blue-500">App</div>
+
+    <Router>
+      <Routes>
+        <Route path="/home" element={<HomePage />} />
+      </Routes>
+    </Router>
   )
 }
 
---
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import recipesData from "../data.json";
+
+
+function HomePage() {
+
+    const [recipes, setRecipes] = useState([]);
+
+    useEffect(() => {
+        setRecipes(recipesData)
+    }, []);
+
+    return (
+        <body className="bg-white">
+            <main className="max-w-6xl mx-auto px-4 py-8">
+                <h1 className="text-3xl font-bold mb-12 text-center">🍽 Recipe Sharing Platform</h1>
+                <div className="grid justify-center gap-6 grid-cols-3">
+                    {recipes.map(recipe => (
+                        <div className="bg-white rounded-lg shadow-md overflow-hidden hover:shadow-lg transition" key={recipe.id}>
+                            <img
+                                alt={recipe.title}
+                                src={recipe.image}
+                                className="w-full h-56 object-cover">
+                            </img>
+                            <div className="p-4">
+                                <h2 className='text-xl front-semibold mb-2'>{recipe.title}</h2>
+                                <p className="text-gray-600 text-sm mb-4">{recipe.summary}</p>
+                            </div>
+                            <a href="" className='inline-block px-4 py-2 bg-yellow-500 text-white rounded m-4 hover:bg-yellow-600'>
+                                View Recipe
+                            </a>
+                        </div>
+                    ))}
+                </div>
+            </main>
+        </body>
+    )
+}
+
+export default HomePage;
